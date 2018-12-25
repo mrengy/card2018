7c77b4b6ed0184e6e43c403ad09ae3895e462e98
Mike Eng <mike.eng@adhocteam.us>
2018-12-25 00:09:08 -0500

    moving snail initially complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 $(document).foundation().ready( function(){
-  $('#lightning').click(function(){$('#snail-container').addClass('moved')});
+  //Move the snail
+
+  var containingA = $('#lightning').parent('a');
+  console.log(containingA);
+  $('#lightning').click(function(e){
+    //make lightning not clickable
+    $('#lightning').unwrap('a');
+    //stop the browser's click action
+    e.preventDefault();
+    //add class to the snail container
+    $('#snail-container').addClass('moved');
+  });
+
+  $('#snail').parent().on( 'transitionend', function() {
+    $('#snail-container').removeClass('moved');
+    $('#lightning').wrap(containingA);
+  });
 });
